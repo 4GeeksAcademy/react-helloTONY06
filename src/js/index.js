423418e9146a888e07729808685dbd0ff73e3b27
Tony1406 <142957069+Tony1406@users.noreply.github.com>
2023-11-29 15:03:10 +0000

    SimpleCounter

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,27 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
+import SecondsCounter from "./component/SecondsCounter";
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let unidad = 0;
+let decimal = 0;
+let centena = 0;
+let miles = 0;
+setInterval(() => {
+    unidad ++;
+    if (unidad > 9){
+        unidad = 0
+        decimal ++
+    } 
+    if (decimal > 9){
+        decimal = 0
+        centena ++
+    } 
+    if(centena > 9){
+        centena = 0
+        miles ++
+    }
+    ReactDOM.render(<SecondsCounter unidad={unidad} decimal={decimal} centena= {centena} miles={miles}/>, document.querySelector("#app"));
+
+}, 1000) 
